5023ce01c9c165c355bebd106ac9975dfc38de6a
Paolo Landeros <landerospaolo@gmail.com>
2024-05-30 16:58:16 -0400

    login form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,8 +1,28 @@
+import '../stylesheets/login.css'
 
 const Login = () => {
     return (
-        <div>
-            Hola Login
+        <div className="container mt-5 pt-5">
+            <div className="row">
+                <div className="col-12 col-sm-8 col-md-6 m-auto">
+                    <div className="card border-0 shadow">
+                        <div className="card-body">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
+                            <form action="">
+                                <input type="text" name="" id="" className="form-control my-4 py-22" placeholder="Username" />
+                                <input type="text" name="" id="" className="form-control my-4 py-22" placeholder="Password" />
+                                <div className="text-center">
+                                    <button className="btn btn-primary">Login</button>
+                                    <a href="/" className="nav-link mt-3">Volver</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
